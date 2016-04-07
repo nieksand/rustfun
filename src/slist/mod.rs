c550689a293fbefa81f3820559e810523e4d0945
Niek Sanders <niek.sanders@gmail.com>
2016-04-07 15:06:21 -0700

    Slist implementation.

--- src/slist/mod.rs
+++ src/slist/mod.rs
@@ -1,6 +1,27 @@
 /*
  * Singly Linked List Implementation.
+ *
+ * Design choices:
+ *
+ *   - [TODO] Wrapper struct for list rather than using head node directly.
+ *            Gives us a place to store list length and changes that operation
+ *            from O(n) to O(1).
+ *
+ *   - [TODO] Templatized list/node type to embed contained value directly in
+ *            list nodes.  Avoids unneeded pointer redirection.
+ *
  */
+
+pub struct Slist {
+	head:	Option<Box<Node>>,
+	len:	usize,
+}
+
+impl Slist {
+	pub fn len(&self) -> usize {
+		self.len
+	}
+}
 
 pub struct Node {
 	val:	i32,
@@ -13,7 +34,9 @@ impl Node {
 	}
 }
 
-
+/*
+ * Convert vector to linked list representation.
+ */
 pub fn vec_to_list(dat: &Vec<i32>) -> Option<Box<Node>> {
 	if dat.len() == 0 {
 		return None;
@@ -28,6 +51,9 @@ pub fn vec_to_list(dat: &Vec<i32>) -> Option<Box<Node>> {
 	return prev_node;
 }
 
+/*
+ * Get length of list.
+ */
 pub fn list_len(n: &Option<Box<Node>>) -> usize {
 	let mut len = 0;
 	let mut cur = n;
@@ -41,6 +67,9 @@ pub fn list_len(n: &Option<Box<Node>>) -> usize {
 	len
 }
 
+/*
+ * Check if element in list.
+ */
 pub fn list_contains(n: &Option<Box<Node>>, val: i32) -> bool {
 	let mut found = false;
 	let mut cur = n;
